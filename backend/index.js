553ca40daa8ccbe9317de9072717b58181baf964
Вячеслав <slavtas@gmail.com>
2025-01-30 10:08:25 +0200

    update

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path"
+import { fileURLToPath } from "url";
 
 import routes from "./routes/index.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
@@ -14,12 +19,18 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api-v1", routes);
 
-app.use("*", (req, res) => {
-  res.status(404).json({
-    status: "404 Not Found",
-    message: "Route Not Found",
-  });
-});
+// app.use("*", (req, res) => {
+//   res.status(404).json({
+//     status: "404 Not Found",
+//     message: "Route Not Found",
+//   });
+// });
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+app.get ("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"))
+}
+);
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
