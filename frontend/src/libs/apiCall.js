2f6b66622e6bb31ea235d9ff3dae4d855f03837d
Вячеслав <slavtas@gmail.com>
2025-01-30 10:50:58 +0200

    update

--- frontend/src/libs/apiCall.js
+++ frontend/src/libs/apiCall.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const API_URL = `http://localhost:5000/api-v1`;
+const url = import.meta.env.VITE_BASE_URL || 'http://localhost:5000'
+
+//const API_URL = `http://localhost:5000/api-v1`;
+const API_URL = new URL('api-v1', API_URL)
+console.log(API_URL)
 
 const api = axios.create({
   baseURL: API_URL,
